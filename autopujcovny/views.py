2a615c1f478c936b2b4b74a7cc9ae0a1d34ac44c
lucny <lucny@sspu-opava.cz>
2023-04-19 22:58:59 +0200

    04 - uvodni stranka

--- autopujcovny/views.py
+++ autopujcovny/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Autopujcovna, Hodnoceni
+
+
+def index(request):
+    context = {
+        'nadpis': 'Autopůjčovny ve vašem okolí',
+        'autopujcovny': Autopujcovna.objects.all(),
+        'hodnoceni': Hodnoceni.objects.order_by('-cas')[:3]
+    }
+    return render(request, 'index.html', context=context)
--- mat_autopujcovny/urls.py
+++ mat_autopujcovny/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('autopujcovny/', include('autopujcovny.urls')),
+    path('', RedirectView.as_view(url='autopujcovny/')),
 ]
 
 if settings.DEBUG:
